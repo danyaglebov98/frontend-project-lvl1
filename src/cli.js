046ecbad8319c8dec00971924a84035135a2132e
Danil <danil.glebov@dualbootpartners.com>
2021-01-05 18:50:28 +0400

    add new functional for brain-games

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const whatsName = () => {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`)
+}
+
+export {whatsName as default};
