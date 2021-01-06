ed69d8690909dbe33b8019f0c7874563c6df0310
Danil <danil.glebov@dualbootpartners.com>
2021-01-06 17:44:20 +0400

    add new game brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import whatsName from '../src/cli.js';
+
+const randomize = (max) => Math.floor(Math.random() * Math.floor(max));
+const name = whatsName();
+const rightAnswer = 'Correct!';
+const wrongAnswer = `'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`;
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+for (let i = 0; i < 3; i += 1) {
+  const randomNumber = randomize(100);
+  console.log(`Question: ${randomNumber}`);
+  const yourAnswer = readlineSync.question('Your Answer: ');
+  const answer = randomNumber % 2 === 0 ? 'yes' : 'no';
+  if (answer !== yourAnswer) {
+    console.log(wrongAnswer);
+    break;
+  }
+  console.log(rightAnswer);
+
+  if (i === 2) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
